bd03664598bdf08bae8d782daed0fd8d580f7455
GabrieL_Lauro <gabriellauromelo@gmail.com>
2025-11-22 18:13:57 -0300

    remove backend python

--- backendPython/utils/responses.py
+++
@@ -1,8 +0,0 @@
-from flask import make_response
-import json
-
-def json_response(data, status=200):
-    """Cria uma resposta JSON com encoding UTF-8"""
-    response = make_response(json.dumps(data, ensure_ascii=False, indent=2), status)
-    response.headers['Content-Type'] = 'application/json; charset=utf-8'
-    return response
